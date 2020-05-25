e5430560041a13feedf8e0b533ef50a19af24efd
Riverbui <4911928+rocket0634@users.noreply.github.com>
2020-05-25 15:28:31 -0700

    Initial commit
    - Key source: https://assetstore.unity.com/packages/3d/handpainted-keys-42044
    - Module idea: Ryaninator
    - Knob model: hockeygoalie78
    - Google Docs manual: https://docs.google.com/document/d/1IPF8P_5t5rUEugxYfHlmVEHSiHSbJV8KBCEAHssu2cQ/edit

---
+++ Assets/Scripts/TurnAKey.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Reflection;
+
+public class TurnAKey : MonoBehaviour {
+
+    private static int _moduleIDCounter = 1;
+    private int _moduleID;
+    public KMBombInfo Info;
+    public KMBombModule Module;
+    public KMAudio Audio;
+    private bool active;
+
+    void Start () {
+        _moduleID = _moduleIDCounter++;
+        Module.OnActivate += delegate () { active = true; };
+    }
+}
